a1901e3d057eb67f982af1b53685f8c313d89426
Akash <mallikarjunsalaki0@gmail.com>
2026-06-03 10:30:09 +0000

    Day 2

---
+++ CountEMore3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int a,count=0,r,n;
+	printf("Enter the Number : ");
+	scanf("%d",&a);
+	printf("\nThe given numbe is : %d\n",a);
+	printf("Enter random single digit numbere : ");
+	scanf("%d",&n);
+	printf("\n");
+	for(int i=a;i;i=i/10)
+	{
+		r=i%10;
+		if(r%2!=0)
+		{
+			if(3<n)
+			count++;
+		}
+	}
+	printf("Count of Odd number if it is grater than 3 : %d\n",count);
+
+}
+
